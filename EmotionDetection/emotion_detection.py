5ae068f3875029ab826b47cb2ca22d86799ac547
impressiondirect360-crypto <impressiondirect360@gmail.com>
2026-03-24 16:31:00 -0500

    Create emotion_detection.py

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+      url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+      header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+      myobj = { "raw_document": { "text": text_to_analyze } }
+      response = requests.post(url, json = myobj, headers=header)
+
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200:
+              emotions = formatted_response['emotionPredictions'][0]['emotion']
+              dominant_emotion = max(emotions, key=emotions.get)
+              result = {
+                  'anger': emotions['anger'],
+                  'disgust': emotions['disgust'],
+                  'fear': emotions['fear'],
+                  'joy': emotions['joy'],
+                  'sadness': emotions['sadness'],
+                  'dominant_emotion': dominant_emotion
+              }
+elif response.status_code == 400:
+          result = {
+                        'anger': None,
+                        'disgust': None,
+                        'fear': None,
+                        'joy': None,
+                        'sadness': None,
+                        'dominant_emotion': None
+          }
+
+    return result
